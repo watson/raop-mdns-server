4bcfe51ae49e073634874ec0fc067ed9b6523c67
Thomas Watson Steen <w@tson.dk>
2015-09-05 22:29:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# raop-mdns-server
+
+A RAOP mDNS broadcast server.
+
+This moduel is not a RAOP server in it self, but mearly broadcasting its
+address (the mac/ip of the machine running this Node module) using
+multicast DNS.
+
+[![Build status](https://travis-ci.org/watson/raop-mdns-server.svg?branch=master)](https://travis-ci.org/watson/raop-mdns-server)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+
+## Installation
+
+```
+npm install raop-mdns-server
+```
+
+## Usage
+
+```js
+var mdns = require('raop-mdns-server')
+
+var opts = {
+  name: 'My RAOP Server',
+  version: '1.0.0',
+  port: 5000
+}
+
+mdns(opts, function (err) {
+  if (err) throw err
+  console.log('RAOP server is being advertised')
+})
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict'
+
+var mdns = require('mdns')
+var getmac = require('getmac')
+var pkg = require('./package')
+var debug = require('debug')(pkg.name)
+
+// required options:
+// - name
+// - version (not required if txt is given)
+// - port
+//
+// optional options:
+// - txt
+// - mac
+module.exports = function (opts, cb) {
+  if (opts.mac) {
+    broadcast(opts.mac)
+  } else {
+    getMac(function (err, mac) {
+      if (err) return cb(err)
+      broadcast(mac)
+    })
+  }
+
+  function broadcast (mac) {
+    var fullName = mac.toUpperCase().replace(/:/g, '') + '@' + opts.name
+    var txt = opts.txt || generateTxt(mac)
+    debug('Advertising RAOP server on port %d', opts.port, fullName)
+
+    mdns
+      .createAdvertisement(mdns.tcp('raop'), opts.port, { name: fullName, txtRecord: txt })
+      .start()
+
+    process.nextTick(function () {
+      cb(null, mac)
+    })
+  }
+
+  function generateTxt (mac) {
+    var model = opts.name + opts.version.split('.').slice(0, -1).join(',')
+
+    return {
+      txtvers: '1',     // TXT record version 1
+      ch: '2',          // audio channels: stereo
+      cn: '0,1,2,3',    // audio codecs
+      et: '0,3,5',      // supported encryption types
+      md: '0,1,2',      // supported metadata types
+      pw: 'false',      // does the speaker require a password?
+      sr: '44100',      // audio sample rate: 44100 Hz
+      ss: '16',         // audio sample size: 16-bit
+      tp: 'UDP',        // supported transport: TCP or UDP
+      vs: opts.version, // server version
+      am: model         // device model
+    }
+  }
+}
+
+function getMac (cb) {
+  // TODO: This doesn't necessarily find the MAC address associated with the
+  // interface that the server is broadcasting on
+  debug('Getting server MAC address')
+  getmac.getMac(function (err, mac) {
+    if (err) return cb(err)
+    debug('Found MAC address', mac)
+    cb(null, mac)
+  })
+}
